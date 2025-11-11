9a229d077b0a2b1c3e6f649757c9c32ff35bf676
M. DICKY HAIKAL ALY <dickyhaikal12@gmail.com>
2025-11-11 15:11:58 +0700

    feat: add Sekolah management functionality
    - Updated package name in package-lock.json to lowercase.
    - Added SekolahController for handling Sekolah resources.
    - Created SekolahRequest for validation of Sekolah data.
    - Implemented SekolahPolicy for authorization checks.
    - Developed SekolahService for business logic related to Sekolah.
    - Created views for listing and adding Sekolah.
    - Integrated search functionality in Sekolah index view.
    - Added routes for Sekolah resource management.

---
+++ resources/views/pages/admin/sekolah/create.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.appadmin')
+
+@section('title', 'Tambah Sekolah')
+
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}" class="text-decoration-none">Manajemen Sekolah</a>
+    </li>
+    <li class="breadcrumb-item active" aria-current="page">Tambah Sekolah</li>
+@endsection
+
+@section('content')
+    <div class="card shadow-sm border-0 mb-4 p-3">
+        <div class="card-header bg-white border-0 d-flex justify-content-between align-items-center">
+            <h5 class="mb-0 fw-semibold fs-4">Tambah Sekolah</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('admin.sekolah.store') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama</label>
+                    <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama"
+                        name="nama" value="{{ old('nama') }}" required autofocus placeholder="Masukkan nama Sekolah">
+                    @error('nama')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Alamat</label>
+                    <input type="text" class="form-control @error('alamat') is-invalid @enderror" id="alamat"
+                        name="alamat" required placeholder="Masukkan alamat Institusi">
+                    @error('alamat')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="d-flex justify-content-between mt-4">
+                    <a href="{{ route('admin.institutes.index') }}" class="btn btn-secondary">
+                        <i class="bi bi-arrow-left"></i> Kembali
+                    </a>
+                    <button type="submit" class="btn btn-success">
+                        <i class="bi bi-save me-1"></i> Simpan
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
