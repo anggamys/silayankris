fa9f709f4777ebe3611f96bd065fa4c64114d6c4
M. DICKY HAIKAL ALY <dickyhaikal12@gmail.com>
2025-11-13 12:36:16 +0700

    feat: add nullable profile_photo_path column to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_photo_path')->nullable();
             $table->enum('role', ['admin', 'guru', 'pengurus-gereja'])->default('guru');
             $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
             $table->rememberToken();
